f0a29e7fc06e7b4be8d17e6a92115f98f18700d3
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-05 16:40:35 -0300

    Segundo commit: objetos personas. SUPER BASICO pero funcional

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+    Persona persona1 = new Persona();
+    persona1.nombre="Teo";
+    persona1.apellido="Fernández";
+    persona1.edad=21;
 
+    System.out.println("La persona 1 es: " + persona1.apellido + " " + persona1.nombre + " de " + persona1.edad+" años.");
+
+    Persona persona2 = new Persona();
+    persona2.nombre = "Lara";
+    persona2.apellido = "Fernández";
+    persona2.edad = 19;
+
+    System.out.println("La persona 2 es: " + persona2.apellido + " " + persona2.nombre + " de " + persona2.edad+" años.");
     }
 }
