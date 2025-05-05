9b2fe5e6c8de7469e6e081ce060a088547fa65b6
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-05 17:08:29 -0300

    Tercer Commit: Agregue metodos y tambien estoy agregando la clase 'Carrera', que por ahora no hace nada ni tiene datos. TODO COMPLETAMENTE FUNCIONAL

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,18 @@ public class Main {
     persona1.apellido="Fernández";
     persona1.edad=21;
 
-    System.out.println("La persona 1 es: " + persona1.apellido + " " + persona1.nombre + " de " + persona1.edad+" años.");
+    System.out.println("La persona 1 es: " + persona1.darNombreCompleto() + " de " + persona1.edad+" años.");
 
     Persona persona2 = new Persona();
     persona2.nombre = "Lara";
     persona2.apellido = "Fernández";
     persona2.edad = 19;
 
-    System.out.println("La persona 2 es: " + persona2.apellido + " " + persona2.nombre + " de " + persona2.edad+" años.");
+    System.out.println("La persona 2 es: " + persona2.darNombreCompleto() + " de " + persona2.edad+" años.");
+
+    String saludo = "Noelia";
+    System.out.println(persona1.enviarSaludo(saludo));
+    System.out.println(persona2.enviarSaludo("Ezequiel"));
+
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,17 @@
 public class Persona {
+
+    //Atributo. "Características de un objeto".
     String nombre;
     String apellido;
     int edad;
+
+    //Metodos (Funciones)
+    public String darNombreCompleto(){
+        return apellido + ", " + nombre;
+    }
+
+    public String enviarSaludo(String saludo) {
+        if(edad >40) return "Buenos días, querido " + saludo;
+        return "Hola, ¿Cómo estás " + saludo + "?";
+    }
 }
