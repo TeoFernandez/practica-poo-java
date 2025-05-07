c14abfd28cc36afb2bcd62f2e9b387b239011652
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-07 00:42:12 -0300

    Implemento Getters y Setters en la clase 'Persona' con la funcionalidad de '.toUpperCase()' en los getters de 'nombre' y 'apellido'. Luego cargo datos e imprimo todo en 'Main.java'

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona("Pedro","Pascal",48);
+        Persona persona1 = new Persona("Carlos","Gardel",23);
 
-        System.out.println(persona1.darNombreCompleto() + " tiene " + persona1.edad + " años.");
+        persona1.setNombre("Ricardo");
+        persona1.setApellido("Darín");
+        persona1.setEdad(60);
+
+        System.out.println(persona1.darNombreCompleto() + " tiene " + persona1.getEdad() + " años.");
 
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,9 +1,9 @@
 public class Persona {
 
     //Atributo. "Características de un objeto".
-    String nombre;
-    String apellido;
-    int edad;
+    private String nombre;
+    private String apellido;
+    private int edad;
 
     //Constructor
     public Persona(String nombre, String apellido, int edad){
@@ -15,11 +15,38 @@ public class Persona {
 
     //Metodos (Funciones)
     public String darNombreCompleto(){
-        return apellido + ", " + nombre;
+        return getApellido() + ", " + getNombre();
     }
 
     public String enviarSaludo(String saludo) {
         if(edad >40) return "Buenos días, querido " + saludo;
         return "Hola, ¿Cómo estás " + saludo + "?";
     }
+
+    //Getters (Devuelven) and Setters (fijan - CARGAN EL DATO)
+    public String getNombre(){
+        return nombre.toUpperCase();
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido.toUpperCase();
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
 }
